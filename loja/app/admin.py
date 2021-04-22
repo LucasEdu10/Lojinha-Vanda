fabcd8bb96b78633958f1d713843f101646ae162
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2021-04-22 01:49:00 -0300

    Added model fabric in projeto

--- loja/app/admin.py
+++ loja/app/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Fabric
+from .models import Product, ProductType, Fabric
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name_product', 'available_product')
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display=('name_product',)
 
 class FabricAdmin(admin.ModelAdmin):
-    list_display=('name_fabric', 'available_fabric')
-    
+    list_display=('cod_fabric','name_fabric', 'type_fabric', 'available_fabric')
 
     
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductType, ProductTypeAdmin)
 admin.site.register(Fabric, FabricAdmin)
 # Register your models here.
