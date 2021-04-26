f219a628d9d6710e958b5e664119ced214b03792
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2021-04-26 00:49:36 -0300

    Ajusted project for deploy in heroku

--- loja/settings.py
+++ loja/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$gj#0pmib-hok6o&njw4--$p$-^$r^&a@oozf%i=kjyzt7%7yr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://loja-vanda.herokuapp.com/']
 
 
 # Application definition
@@ -125,3 +127,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+django_heroku.settings(locals())
